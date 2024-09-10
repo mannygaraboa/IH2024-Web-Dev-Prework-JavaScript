b5f91b4039c0a585ea505ec3ef08624aa3bd4e3c
Manuel Garaboa <mannygaraboa@gmail.com>
2024-09-09 23:36:10 -0400

    JS - Introduction and Syntax: Lesson Complete ' '

--- JS-Introduction-and-Syntax/script.js
+++ JS-Introduction-and-Syntax/script.js
@@ -3,3 +3,11 @@ function hello(name) {
 }
 
 hello('World!');
+
+console.log("My name is Bob, and I'm writing JavaScript");
+
+// a short, one-line comment
+
+/* this is a long, multi-line comment
+    Hopefully one day I'll appreciate
+    writingall these comments :) */
