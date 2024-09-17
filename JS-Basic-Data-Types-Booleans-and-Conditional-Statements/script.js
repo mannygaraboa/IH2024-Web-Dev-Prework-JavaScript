8970a39c76b405982a1784d0cb49c68edf91ef47
Manuel Garaboa <mannygaraboa@gmail.com>
2024-09-16 23:41:13 -0400

    JS - Basic Data Types - Booleans & Conditional Statements: Lesson Start

---
+++ JS-Basic-Data-Types-Booleans-and-Conditional-Statements/script.js
@@ -0,0 +1,39 @@
+// **************** //
+// *** Booleans *** //
+// **************** //
+
+let powerOn = true;         // ==> boolean
+let isRaining = false;      // ==> boolean
+
+// ************************* //
+// *** Logical Operators *** //
+// ************************* //
+
+    // ** AND(&&) ** //
+        
+    // ** OR(||) ** //
+
+    // ** NOT(!) ** //
+
+2 == '2';                   // ==> true
+2 === '2';                  // ==> false
+2 === 2;                    // ==> true
+
+// ******************************* //
+// *** Conditional Statementes *** //
+// ******************************* //
+
+    // ** IF Statement ** //
+    /*
+        if (condition) {
+            
+        }
+    */
+
+    if (age = 18) {
+        // Somthing happens here
+    }
+
+    if (companyName === 'Ironhack') {
+        // Something happens here
+    }
