a60dbad5a56db5a4d8aeeb5a6e212aa5ebd59736
Manuel Garaboa <mannygaraboa@gmail.com>
2024-09-17 23:32:59 -0400

    JS - Basic Data Types - Booleans & Conditional Statements: Progress #1

--- JS-Basic-Data-Types-Booleans-and-Conditional-Statements/script.js
+++ JS-Basic-Data-Types-Booleans-and-Conditional-Statements/script.js
@@ -30,10 +30,56 @@ let isRaining = false;      // ==> boolean
         }
     */
 
+    let age = 18;
+    let companyName = 'Ironhack';
+    
     if (age = 18) {
         // Somthing happens here
     }
 
     if (companyName === 'Ironhack') {
         // Something happens here
+    }
+
+    // These 2 are the same:
+        let isIronhackStudent = true;
+        if (isIronhackStudent === true) {
+            // Something happens here
+        }
+
+        if(isIronhackStudent) {
+            // Something happens here
+        }
+
+        // Opposite case: meaning if the condition is false
+        if (!isIronhackStudent) {
+            // Something happens here
+        }
+
+    let isIronHackStudent2 = true;
+    let hardWork = 9;
+
+    if (isIronHackStudent2) {
+        console.log("Welcome to Ironhack :)");
+    }
+        if (hardWork > 8) {
+            console.log("You will be a great developer!");
+        }
+
+    // NOT
+    let doesCode = true;
+    if (!doesCode) {
+        console.log("Ironhack is the right place to be!");
+    }
+
+    // AND
+    let lovesParty = true;
+    if (doesCode && lovesParty) {
+        console.log("You seem to be an Ironhacker!");
+    }
+
+    // OR
+    let framework = "Express";
+    if (doesCode && (framework === "Express" || framework === "React")) {
+        console.log("You are a smart developer!");
     }
