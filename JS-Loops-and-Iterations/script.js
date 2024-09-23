834092b4193fcc6ae434144aa8049fe53670ea8d
Manuel Garaboa <mannygaraboa@gmail.com>
2024-09-23 16:51:54 -0400

    JS - Loops and Iterations: Lesson Start

---
+++ JS-Loops-and-Iterations/script.js
@@ -0,0 +1,17 @@
+// ****************** //
+// *** While Loop *** //
+// ****************** //
+
+/*
+    while (condition) {
+        code to be executed while the condition is true
+    }
+*/
+
+let counter = 1;
+
+while (counter <= 10) {
+    console.log(counter);
+    counter += 1;
+}
+
