6f812bcb5beef1b4c93acb0f3e46fc29bbd1a5e8
Manuel Garaboa <mannygaraboa@gmail.com>
2024-09-25 22:19:12 -0400

    JS - Basic Data Types - Arrays: Lesson Start

---
+++ JS-Basic-Data-Types-Arrays/script.js
@@ -0,0 +1,16 @@
+let animalsArray1 = [];
+
+const animalsArray2 = ['dog', 'cat', 'snake'];
+
+const animalsArray3 = ["dog", "cat", "fish"];
+
+const myDog = animalsArray3[0];
+console.log(myDog);
+
+const myFish = animalsArray3[2];
+console.log(myFish);
+
+// Returns undefined when there's no element with that index:
+const myUndefinedElement = animalsArray3[3];
+console.log(myUndefinedElement);
+
