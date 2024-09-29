40823835109e2927dd84dded7a9c0e0e03f3622c
Manuel Garaboa <mannygaraboa@gmail.com>
2024-09-28 23:53:35 -0400

    JS - Basic Data Types - Arrays: Lesson Progress #3

--- JS-Basic-Data-Types-Arrays/script.js
+++ JS-Basic-Data-Types-Arrays/script.js
@@ -22,34 +22,70 @@ console.log(`The length of the animalsArray3 is: ${animalsArray3.length}`);
 
 const lastIndex = animalsArray3.length - 1;
 const lastElement = animalsArray3[lastIndex];
-console.log(lastElement);
+console.log(lastElement + "\n");
 
     // ** Adding and Removing Elements ** //
 
     // Adding an element (End of Array):
+    console.log("Adding an element (End of Array):");
     const animalsArray4 = ["dog", "cat", "fish"];
     console.log(`Before: ${animalsArray4}`);
 
     animalsArray4.push("lizard");
-    console.log(`After: ${animalsArray4}`);
+    console.log(`After: ${animalsArray4}\n`);
+
 
     // Adding an element (Beginning of Array):
+    console.log("Adding an element (Beginning of Array):");
     const animalsArray5 = ["dog", "cat", "fish"];
     console.log(`Before: ${animalsArray5}`);
 
     animalsArray5.unshift("whale");
-    console.log(`After: ${animalsArray5}`);
+    console.log(`After: ${animalsArray5}\n`);
+
 
     // Removing an element (End of Array):
+    console.log("Removing an element (End of Array):");
     const animalsArray6 = ["dog", "cat", "fish"];
     console.log(`Before: ${animalsArray6}`);
 
     animalsArray6.pop();
-    console.log(`After: ${animalsArray6}`);
+    console.log(`After: ${animalsArray6}\n`);
+
 
     // Removing an element (Beginning of Array):
+    console.log("Removing an element (Beginning of Array):");
     const animalsArray7 = ["dog", "cat", "fish"];
     console.log(`Before: ${animalsArray7}`);
 
     animalsArray7.shift();
-    console.log(`After: ${animalsArray7}`);
+    console.log(`After: ${animalsArray7}\n`);
+
+
+    // Deleting an element
+    console.log("Deleting an element:");
+    const animalsArray8 = ["dog", "cat", "fish"];
+    console.log(`Before: ${animalsArray8}`);
+
+    delete animalsArray8[0];lastElement;
+    console.log(`After: ${animalsArray8}\n`);                   // ==> [undefined, "cat", "fish"]
+
+
+    // Splicing an element
+        console.log("Splicing an elemenet:");
+        const animalsArray9 = ["dog", "cat", "fish", "lizard", "whale", "cheetah"];
+        console.log(`Original: ${animalsArray9}`);
+        console.log("------");
+
+        // From the first element, remove one going forward:
+        animalsArray9.splice(0, 1);
+        console.log(`From the first element, remove one going forward: ${animalsArray9}`);
+
+        // From the third element, remove two going forward:
+        animalsArray9.splice(2, 2);
+        console.log(`From the third element, remove two going forward: ${animalsArray9}`);
+
+        // Inserting a third arguement:
+        animalsArray9.splice(0, 1, "Something else");
+        console.log(`If we pass a third arguement: ${animalsArray9}\n`);
+
