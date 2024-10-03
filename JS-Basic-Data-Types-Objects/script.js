88554d895f1430f57d42b88163fa8a2f06c38570
Manuel Garaboa <mannygaraboa@gmail.com>
2024-10-03 10:30:51 -0400

    JS - Basic Data Types - Objects: Lesson Progress #3

--- JS-Basic-Data-Types-Objects/script.js
+++ JS-Basic-Data-Types-Objects/script.js
@@ -39,7 +39,8 @@ const student = {
 
 let olympicRecords ={
     athletics100Men: 'Justin Gatlin',
-    athleticsLongJumpMen: 'Mike Powel'
+    athleticsLongJumpMen: 'Mike Powel',
+    swimming200Men: 'Michael Phelps'
 };
 
 // ************************ //
