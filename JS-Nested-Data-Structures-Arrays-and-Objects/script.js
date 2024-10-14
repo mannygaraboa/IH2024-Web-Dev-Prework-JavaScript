ea06198a863b91fa2ce5e2c6eb1d71b4bbe4b555
Manuel Garaboa <mannygaraboa@gmail.com>
2024-10-13 22:56:33 -0400

    JS - Nested Data Structures - Arrays and Objects: Extra Practice #3

--- JS-Nested-Data-Structures-Arrays-and-Objects/script.js
+++ JS-Nested-Data-Structures-Arrays-and-Objects/script.js
@@ -202,3 +202,30 @@ console.log(basic.frameworks[1].list[0]);
 
     // The least familiar topic
     console.log(course2.topics[4]);
+
+    // ** Given the object with nested objects in it, print: ** //
+
+    let student = {
+        firstName: 'Ana',
+        lastName: 'Blair',
+        course: {
+            name: 'Web Development',
+            type: 'part-time'
+        },
+        attendedIn: 'Madrid',
+        address: {
+            street: 'Happy Street',
+            number: 123,
+            city: 'Barcelona',
+            zip: '08015',
+            country: 'Spain'
+        }
+    }
+    // console.log(???) ==> Web Development
+    console.log(student.course.name);
+
+    // console.log(???) ==> Happy Street
+    console.log(student.address.street);
+
+    // console.log(???) ==> Ana moved from Barcelona to Madrid to take part-time Web Development course.
+    console.log(`${student.firstName} moved from ${student.address.city} to ${student.attendedIn} to take ${student.course.type} ${student.course.name} course.`);
