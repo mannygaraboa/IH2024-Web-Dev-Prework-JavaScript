47c1bd974348b0717502bca8c7f79cf618d8e2c5
Manuel Garaboa <mannygaraboa@gmail.com>
2024-10-17 23:54:39 -0400

    JS - Functions: Lesson Progress #1

--- JS-Functions/script.js
+++ JS-Functions/script.js
@@ -1,5 +1,33 @@
+// ** Syntax ** //
+    // function <name> ([<argument_list>]) {
+    //     <instructions>
+
+    //     [return <expression>;]
+    // }
+
 function sayHello() {
     console.log('Hello!');
 }
 
 sayHello()
+
+function sayHello2(language) {
+    switch (language) {
+        case 'fr':
+            console.log("Bonjour!");
+            break;
+        case 'ger':
+            console.log("Hallo!");
+            break;
+        case 'esp':
+            console.log("Hola!");
+            break;
+        default:
+            console.log("Hello!");
+    }
+}
+
+sayHello2('fr');
+sayHello2('ger');
+sayHello2('esp');
+sayHello2();
