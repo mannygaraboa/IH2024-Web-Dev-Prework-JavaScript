ac1e3f06f0a1e0d2385cf0de865eb3e9eca6fe3a
Manuel Garaboa <mannygaraboa@gmail.com>
2024-10-23 11:25:16 -0400

    JS - Functions | Guided Example & Practice: Lesson Start

---
+++ JS-Functions-Guided-Example-and-Practice/script.js
@@ -0,0 +1,12 @@
+// ********************** //
+// *** Guided Example *** //
+// ********************** //
+
+function calculator(num1, num2, operator) {
+    let result;
+    switch (operator) {
+        case "+":
+            result = num1 + num2;
+            break;
+    }
+}
