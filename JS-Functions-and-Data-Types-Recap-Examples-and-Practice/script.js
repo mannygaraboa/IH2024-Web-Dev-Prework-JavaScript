39f5d7da9dff0047c801e0bec30d9834ac44c8ca
Manuel Garaboa <mannygaraboa@gmail.com>
2024-11-06 23:48:02 -0500

    JS - Functions and Data Types: Lesson Progress #1

--- JS-Functions-and-Data-Types-Recap-Examples-and-Practice/script.js
+++ JS-Functions-and-Data-Types-Recap-Examples-and-Practice/script.js
@@ -1,2 +1,22 @@
 let name;
 const meter = 100;
+
+let age = 30;
+
+let isStudent = true;
+let hasDegree = false;
+
+let phone = {
+    brand: 'apple',
+    type: 'iPhone',
+    model: 'X',
+    characteristics: {
+        capacity: '256 GB',
+        size: {
+            height: '5.65 inches',
+            width: '2.79 inches',
+            depth: '0.3 inches'
+        }
+    },
+    chip: ['bionic', 'neural engine', 'embedded motion processor']
+};
