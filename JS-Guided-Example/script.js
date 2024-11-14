aa42d4aa64888ee85fa88293c0d0804665611ba6
Manuel Garaboa <mannygaraboa@gmail.com>
2024-11-13 23:03:01 -0500

    JS - Guided Example: Lesson Start

---
+++ JS-Guided-Example/script.js
@@ -0,0 +1,16 @@
+/*
+    parentArray = [
+        [childArray1],
+        [childArray2],
+        [childArray3]
+    ]
+*/
+
+const player = {
+    x: 1,
+    y: 1
+};
+
+function placeHorizontal(thePlayer, command) {
+    
+}
