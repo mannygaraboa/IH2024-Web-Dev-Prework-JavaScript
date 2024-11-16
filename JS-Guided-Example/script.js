72782f3eb2a064a464209123e38f9834638b8717
Manuel Garaboa <mannygaraboa@gmail.com>
2024-11-15 23:11:15 -0500

    JS - Guided Example: Lesson Progress #1

--- JS-Guided-Example/script.js
+++ JS-Guided-Example/script.js
@@ -12,5 +12,12 @@ const player = {
 };
 
 function placeHorizontal(thePlayer, command) {
-    
+    if (command === 'l') {
+        thePlayer.y -= 1;
+    } else {
+        thePlayer.y += 1;
+    }
+    console.log(`Player has position: x = ${thePlayer.x}, y = ${thePlayer.y}`);
 }
+
+placeHorizontal(player, 'l');
