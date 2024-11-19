5c97e8938d55557885b0927ab4aeb5c96e7c20fe
Manuel Garaboa <mannygaraboa@gmail.com>
2024-11-18 23:59:00 -0500

    JS - Guided Example: Lesson Progress #2

--- JS-Guided-Example/script.js
+++ JS-Guided-Example/script.js
@@ -12,12 +12,18 @@ const player = {
 };
 
 function placeHorizontal(thePlayer, command) {
-    if (command === 'l') {
-        thePlayer.y -= 1;
+    if (thePlayer.y >= 0 && thePlayer.y < 2) {
+        if (command === 'l') {
+            thePlayer.y -= 1;
+        } else if (command === 'r'){
+            thePlayer.y += 1;
+        }
+        console.log(`Player has position: x = ${thePlayer.x}, y = ${thePlayer.y}`);
     } else {
-        thePlayer.y += 1;
+        console.log("You can't place player outside of the board!");
     }
-    console.log(`Player has position: x = ${thePlayer.x}, y = ${thePlayer.y}`);
 }
 
 placeHorizontal(player, 'l');
+placeHorizontal(player, 'l');
+placeHorizontal(player, 'r');
